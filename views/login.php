a56593051cf125bfd33252f128365a8fff85b6e8
marie diallo <mariesarrdiallo@gmail.com>
2019-10-04 09:01:51 +0000

    ma derniere integration

---
+++ views/login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>GesTicketSA</title>
+    <link rel="stylesheet" type="text/css" href=/var/www/html/GesticketSA-table4/public/css/login.css/>  
+</head>
+
+
+<body>
+ <div style="width:right"> 
+    <h1 style="text-align: center;">BIENVENUE</h1>
+    <img class="img-logo" src="../public/img/100x116.png" alt=""> 
+<h2 style="text-align: center;padding-left: 0px;">S'identifier</h2>
+    <div class="login-box">
+        
+        <form action="" method="post" class="login-box">
+            <div class="textbox">  
+                <input type="text" placeholder="E-mail" name="E-mail" value="" >  
+            </div>
+            <div class="textbox"> 
+                <input type="password" placeholder="Mot de passe" name=" " value="" > 
+            </div>
+            <input class="btn" type="submit" name="Connexion" value="connexion">
+        </form>
+    </div>  
+</body>
+
+</html>
