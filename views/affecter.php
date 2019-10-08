91cd4340c9e5e80e1eec49fcacddf2e7ac5b9595
glorie <gloryayivi@gmail.com>
2019-10-08 19:46:22 +0000

    php

--- views/affecter.php
+++ views/affecter.php
@@ -12,26 +12,34 @@
     <div class="div1">  
         <img class="img-logo" src="../public/img/100x116.png" alt=""> 
         <div>
-            <FORM action ="" method="post">
+            <FORM action ="../controlleurs/affecter.php" method="post">
                 <P>
-                    <LABEL class="label" for="session">Session </LABEL>
-                    <select class="select" name="session">
-                        <option value=""> </option>
-                        <option value="presentiel">2017-2018</option>
-                        <option value="online">2018-2019</option>
+                    <LABEL class="label" for="session">Session </LABEL> 
+                      <select class="select" name="session">
+                      <option value=""> </option> 
+                      <option value="1">champ 1</option> 
+                      <option value="2">champ 2</option> 
+                      <option value="3">champ 3</option> 
                       </select><BR><BR>
+
                     <LABEL class="label" for="referentiel">Référentiel</LABEL>
+                    
                         <select class="select" name="referentiel">
                         <option value=""> </option> 
-                        <option value="refdig">Référent digital</option>
-                        <option value="devweb">Développement web</option>
-                        <option value="dataartisan">Data artisan</option>
+                        <option value="1">champ 1</option> 
+                        <option value="2">champ 2</option> 
+                        <option value="3">champ 3</option> 
                         </select><BR><BR>
+
                     <LABEL class="label" for="effectif">Effectif</LABEL>
                     <INPUT type="integer" name="effectif"><BR><BR>
-                    <button class="btn1" type='reset'><a href="creersession.php">Annuler</a></button>
+                    <button class="btn1" type='reset'>Annuler</button>
                     <button class="btn2" type='submit'>Créer</button>
                 </P>
+                <?php
+                if(isset($_GET['erreur'])){
+                echo $_GET['erreur'];}
+                ?>
             </FORM>
         </div>
     </div>  
